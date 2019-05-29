0527b977a6e63580e501f7c3aff99685c845976b
Benjamin Healy <benjamin.healy@ni.com>
2019-05-29 15:25:32 -0500

    Revert "Updated color scheme to be brighter, moved colors to indicator dots instead of text to make things look cleaner."
    This reverts commit 9be43f0c46d7863645b0cda08460d4c01923fa97.

--- NodeColor.py
+++ NodeColor.py
@@ -12,6 +12,6 @@ class NodeColor:
         if (node.is_reference_node):
             self.color = Fore.YELLOW + Style.BRIGHT
         elif (node.children):
-            self.color = Fore.GREEN + Style.BRIGHT
+            self.color = Fore.GREEN + Style.NORMAL
         else:
-            self.color = Fore.BLUE + Style.BRIGHT
+            self.color = Fore.BLUE + Style.NORMAL
