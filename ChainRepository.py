a872a607d8b4e6824a469aeb2833b9f87bcb5729
Christian Nunnally <christiansnunnally@gmail.com>
2019-07-25 21:07:42 -0500

    removed the max number of commits to walk master field

--- ChainRepository.py
+++ ChainRepository.py
@@ -4,10 +4,7 @@ from CommitNode import CommitNode
 from CommitTree import CommitTree
 
 class ChainRepository():
-    number_of_commits_to_walk_master = 200
-
     def __init__(self, repo_path, master_branch_name):
-        self.flatten_merges_into_tree = True
         self.tree = None
         self.repo = Repository(repo_path)
         self.master_branch_name = master_branch_name
@@ -18,12 +15,13 @@ class ChainRepository():
         self.local_feature_branches = []
         self.master_branch = None
         self.commit_name_map = {}
+        self.local_branch_merge_bases_with_master = []
 
         # the order that these initialization methods are called matters.
         self.load_local_branches()
         self.load_master_and_feature_branches()
-        self.generate_master_log()
         self.generate_local_branch_logs_to_master()
+        self.generate_master_log()
         self.build_commit_tree()
 
     def load_local_branches(self):
@@ -39,14 +37,19 @@ class ChainRepository():
             self.local_feature_branches.append(local_branch)
 
     def generate_master_log(self):
-        self.master_log.clear()
-        commit_count = 0
-        for commit in self.repo.walk(self.master_branch.target, GIT_SORT_TOPOLOGICAL):
-            commit_count += 1
+        commits_to_find = list(self.local_branch_merge_bases_with_master)
+        for commit in self.walk_first_parent(self.master_branch.target):
             self.master_log.append(commit)
-            if (commit_count > self.number_of_commits_to_walk_master):
+            if commit in commits_to_find:
+                commits_to_find.remove(commit)
+            if len(commits_to_find) == 0:
                 break
         self.master_log.reverse()
+
+    def walk_first_parent(self, branch):
+        walker = self.repo.walk(branch, GIT_SORT_TOPOLOGICAL)
+        walker.simplify_first_parent()
+        return walker
 
     def generate_local_branch_logs_to_master(self):
         for local_branch in self.local_branches:
@@ -60,33 +63,19 @@ class ChainRepository():
                 self.local_branch_logs_to_master.append(branch_log_to_master)
 
     def generate_branch_log_to_master(self, branch):
-        branch_log_to_master = []
         commit = self.repo.get(branch.target)
-        self.generate_branch_log_to_master_recursive(commit, branch_log_to_master, 0, self.number_of_commits_to_walk_master, None, None)
+        master_commit = self.repo.get(self.master_branch.target)
+        merge_base = self.repo.merge_base(commit.oid, master_commit.oid)
+        self.local_branch_merge_bases_with_master.append(merge_base)
+        branch_log_to_master = []
+
+        for commit in self.walk_first_parent(branch.target):
+            branch_log_to_master.append(commit)
+            if commit == merge_base:
+                break
+
         branch_log_to_master.reverse()
         return branch_log_to_master
-
-    def generate_branch_log_to_master_recursive(self, commit, branch_log, iterations, max_depth_to_search_for_master_commit, stop_commit, fake_parent):
-        if (not stop_commit == None and commit.hex == stop_commit.hex):
-            return
-        if (len(commit.parents) > 0 and not stop_commit == None and commit.parents[0].hex == stop_commit.hex):
-            branch_log.append(commit)
-        else:
-            branch_log.append(commit)
-        if commit in self.master_log:
-            return
-        if (len(commit.parents) == 0):
-            branch_log = []
-            return
-        if iterations > self.number_of_commits_to_walk_master:
-            return
-        
-        if (len(commit.parents) == 2 and self.flatten_merges_into_tree):
-            merge_base = self.repo.merge_base(commit.parents[0].oid, commit.parents[1].oid)
-            new_fake_parent = commit.parents[0].oid
-            self.generate_branch_log_to_master_recursive(commit.parents[1], branch_log, iterations + 1, max_depth_to_search_for_master_commit, merge_base, new_fake_parent)
-
-        self.generate_branch_log_to_master_recursive(commit.parents[0], branch_log, iterations + 1, max_depth_to_search_for_master_commit, None, fake_parent)
 
     def get_local_branch_logs_starting_with_commit(self, commit):
         if (commit in self.local_branch_logs_to_master_by_start_commit):
@@ -115,22 +104,22 @@ class ChainRepository():
 
     def build_commit_tree(self):
         self.tree = CommitTree()
-        master_parent_id = None
+        parent_id = None
         for master_commit in self.master_log:
-            master_commit_name = self.get_combined_branch_name_from_commit(master_commit)
-            master_commit_has_name = self.does_commit_have_name(master_commit)
-            master_parent_node = self.tree.insert(master_parent_id, master_commit, master_commit_name, master_commit_has_name, True)
-            master_parent_id = master_parent_node.commit.id
-            local_branch_logs_from_commit = self.get_local_branch_logs_starting_with_commit(master_commit)
+            node = self.insert_commit_into_tree(master_commit, parent_id, True)
+            parent_id = node.commit.id
 
+            local_branch_logs_from_commit = self.get_local_branch_logs_starting_with_commit(master_commit)
             for local_branch_log in local_branch_logs_from_commit:
-                parent_id = master_parent_id
                 for commit in local_branch_log[1:]:
-                    commit_name = self.get_combined_branch_name_from_commit(commit)
-                    commit_has_name = self.does_commit_have_name(commit)
-                    parent_node = self.tree.insert(parent_id, commit, commit_name, commit_has_name, False)
+                    parent_node = self.insert_commit_into_tree(commit, parent_id, False)
                     parent_id = parent_node.commit.id
         self.tree.refresh_nodes_staleness_status()
+
+    def insert_commit_into_tree(self, commit, parent_id, is_part_of_master):
+        commit_name = self.get_combined_branch_name_from_commit(commit)
+        commit_has_name = self.does_commit_have_name(commit)
+        return self.tree.insert(parent_id, commit, commit_name, commit_has_name, True)
 
     def get_combined_branch_name_from_commit(self, commit):
         names = self.get_commit_names(commit)
@@ -140,3 +129,5 @@ class ChainRepository():
                 combined_names = combined_names + ", "
             combined_names = combined_names + name
         return combined_names
+
+        # merge_base = self.repo.merge_base(commit.parents[0].oid, commit.parents[1].oid)
