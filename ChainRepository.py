00256b5dd526d2543687a3468fc10a16a1c5c037
Christian Nunnally <christiansnunnally@gmail.com>
2019-08-06 21:21:09 -0500

    begin major refactor to treat master like any other branch

--- ChainRepository.py
+++ ChainRepository.py
@@ -8,68 +8,52 @@ class ChainRepository():
     def __init__(self, repo_path, master_branch_name, local_branches_to_include = []):
         self.tree = None
         self.repo = Repository(repo_path)
-        self.master_branch_name = master_branch_name
-        self.local_branch_logs_to_master = []
+        # self.master_branch_name = master_branch_name
+        self.local_branch_logs_to_merge_base = []
         self.local_branches = []
-        self.master_log = []
+        # self.master_log = []
         self.local_feature_branches = []
-        self.master_branch = None
+        # self.master_branch = None
         self.commit_name_map = {}
         self.local_branch_merge_bases_with_master = []
         self.local_branches_to_include = local_branches_to_include
 
         # the order that these initialization methods are called matters.
         self.initialize_branches()
-        self.generate_local_branch_logs_to_master()
-        self.generate_master_log()
+        self.calculate_octopus_merge_base()
+        self.generate_local_branch_logs_to_merge_base()
         self.build_commit_tree()
 
     def initialize_branches(self):
         for local_branch_name in self.repo.branches.local:
-            if len(self.local_branches_to_include) == 0 or local_branch_name == "master" or local_branch_name == "master-real" or local_branch_name in self.local_branches_to_include:
+            if len(self.local_branches_to_include) == 0 or local_branch_name in self.local_branches_to_include:
                 self.local_branches.append(self.repo.branches[local_branch_name])
-        self.master_branch = self.repo.branches[self.master_branch_name]
+        # self.master_branch = self.repo.branches[self.master_branch_name]
 
-    def generate_master_log(self):
-        commit_hexs_to_find = list([i.hex for i in self.local_branch_merge_bases_with_master])
-        for commit in self.walk_from_branch(self.master_branch.target):
-            self.master_log.append(commit)
-            if commit.hex in commit_hexs_to_find: 
-                commit_hexs_to_find.remove(commit.hex)
-            if len(commit_hexs_to_find) == 0:
-                print("Hello")
-                break
-        for x in commit_hexs_to_find:
-            print("chtf: " + x)
-        self.master_log.reverse()
+    def calculate_octopus_merge_base(self):
+        args = ['git', 'merge-base', '--octopus']
+        for local_branch_name in self.repo.branches.local:
+            args.append(local_branch_name)
+        self.octopus_merge_base = subprocess.run(args, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-    def generate_local_branch_logs_to_master(self):
+    def generate_local_branch_logs_to_merge_base(self):
         for local_branch in self.local_branches:
-            if not local_branch.name.endswith(self.master_branch_name):
-                branch_log_to_master = self.generate_branch_log_to_master(local_branch)
-                self.local_branch_logs_to_master.append(branch_log_to_master)
+            branch_log_to_octopus_merge_base = self.generate_branch_log_to_octopus_merge_base(local_branch)
+            self.local_branch_logs_to_merge_base.append(branch_log_to_octopus_merge_base)
 
-    def generate_branch_log_to_master(self, branch):
+    def generate_branch_log_to_octopus_merge_base(self, branch):
         commit = self.repo.get(branch.target)
-        merge_base = self.get_merge_base_with_master(commit)
-        if not merge_base in self.local_branch_merge_bases_with_master:
-            self.local_branch_merge_bases_with_master.append(merge_base)
-
-        branch_log_to_master = []
+        branch_log_to_octopus_merge_base = []
 
         for commit in self.walk_from_branch(branch.target):
-            is_ancestor = subprocess.call(['git', 'merge-base', '--is-ancestor', commit.hex, merge_base.hex])
+            is_ancestor = subprocess.call(['git', 'merge-base', '--is-ancestor', commit.hex, self.octopus_merge_base.hex])
             if (is_ancestor):
-                print(commit.hex + " : " + merge_base.hex)
-            branch_log_to_master.append(commit)
-            if commit.hex == merge_base.hex:
+                print(commit.hex + " : " + self.octopus_merge_base.hex)
+            branch_log_to_octopus_merge_base.append(commit)
+            if commit.hex == self.octopus_merge_base.hex:
                 break
-        branch_log_to_master.reverse()
-        return branch_log_to_master
-    
-    def get_merge_base_with_master(self, commit):
-        master_commit = self.repo.get(self.master_branch.target)
-        return self.repo.merge_base(commit.oid, master_commit.oid)
+        branch_log_to_octopus_merge_base.reverse()
+        return branch_log_to_octopus_merge_base
 
     def walk_first_parent(self, branch):
         walker = self.repo.walk(branch, GIT_SORT_TOPOLOGICAL)
@@ -96,25 +80,32 @@ class ChainRepository():
     def does_commit_have_name(self, commit):
         return commit.hex in self.commit_name_map
 
+    # def build_commit_tree(self):
+    #     self.tree = CommitTree()
+    #     master_parent_id = None
+    #     parent_id = None
+    #     for master_commit in self.master_log:
+    #         node = self.insert_commit_into_tree(master_commit, master_parent_id, True)
+    #         master_parent_id = node.commit.id
+
+    #         if master_commit.hex in [x.hex for x in self.local_branch_merge_bases_with_master]:
+    #             local_branch_logs_from_commit = self.get_local_branch_logs_starting_at_commit(master_commit)
+    #             for local_branch_log in local_branch_logs_from_commit:
+    #                 parent_id = master_parent_id
+    #                 for commit in local_branch_log[1:]:
+    #                     node = self.insert_commit_into_tree(commit, parent_id, False)
+    #                     parent_id = node.commit.id
+
     def build_commit_tree(self):
         self.tree = CommitTree()
-        master_parent_id = None
         parent_id = None
-        for master_commit in self.master_log:
-            node = self.insert_commit_into_tree(master_commit, master_parent_id, True)
-            master_parent_id = node.commit.id
-
-            if master_commit.hex in [x.hex for x in self.local_branch_merge_bases_with_master]:
-                local_branch_logs_from_commit = self.get_local_branch_logs_starting_at_commit(master_commit)
-                for local_branch_log in local_branch_logs_from_commit:
-                    parent_id = master_parent_id
-                    for commit in local_branch_log[1:]:
-                        node = self.insert_commit_into_tree(commit, parent_id, False)
-                        parent_id = node.commit.id
+        for commit in self.local_branch_logs_to_merge_base:
+            node = self.insert_commit_into_tree(commit, parent_id, False)
+            parent_id = node.commit.id
 
     def get_local_branch_logs_starting_at_commit(self, start_commit):
         local_branch_logs_from_commit = []
-        for local_branch_log in self.local_branch_logs_to_master:
+        for local_branch_log in self.local_branch_logs_to_merge_base:
             if local_branch_log[0].hex == start_commit.hex:
                 local_branch_logs_from_commit.append(local_branch_log)
         return local_branch_logs_from_commit
