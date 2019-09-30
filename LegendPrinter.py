abdaa04d8056890a9b7e226c26e470b011abded8
Christian Nunnally <christiansnunnally@gmail.com>
2019-09-29 20:49:47 -0500

    added legend

--- LegendPrinter.py
+++ LegendPrinter.py
@@ -4,9 +4,9 @@ class LegendPrinter:
     def print_legend(self):
         print("Legend:")
         colorer = NodeColorer()
-        print(colorer.color_name("◌◌◌◌ hidden-commits (4)", False, False, True))
+        print(colorer.color_name("◌◌◌◌◌ hidden-commits (4)", False, False, True))
         print(colorer.color_name("◌·6·◌ hidden-commits (8)", False, False, True))
         print(colorer.color_name("●     commit", False, False, False))
         print(colorer.color_name("●     branch", True, False, False))
         print(colorer.color_name("●     current-head", True, True, False))
-        print(colorer.color_name("●     merged-branch", False, False, True)) 
+        print(colorer.color_name("●     merged-branch", False, False, True))
